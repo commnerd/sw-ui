11cb73173061e44c4a952fc14ccafeb44f4eb288
Michael J. Miller <commnerd@gmail.com>
2018-11-29 03:54:55 +0000

    Experimenting with overlay volumes

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --squash -t commnerd/workspace . || docker build -t commnerd/workspace .
+docker build --squash -t commnerd/workspace:overlay . || docker build -t commnerd/workspace:overlay .
